3edd08681333fb0916ad36ee431d8bcf25e799ab
nasirisara <99367926+nasirisara@users.noreply.github.com>
2022-02-09 23:43:18 +0100

    Add project files.

---
+++ if/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace if
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        string stnumber = Console.ReadLine();
+        int number = Convert.ToInt32(stnumber);
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Even");
+        }
+        if(number %2!=0)
+        {
+            Console.WriteLine("Odd");
+        }
+        Console.ReadKey();
+
+    
+        }
+}
+}
